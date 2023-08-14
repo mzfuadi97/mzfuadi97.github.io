f24fe98872352a72e766a634709b092dd8fcaba4
unknown <mzfuadi@gmail.com>
2023-08-14 22:41:05 +0700

    fixed js

---
+++ js/portfolio.js
@@ -0,0 +1,62 @@
+var portfolioFilters = document.querySelectorAll('#portfolio-flters li[data-filter]');
+var selectedFilter = '*'; // Default filter
+
+// Menonaktifkan filter sebelum interaksi "OK"
+function disablePortfolioFilters() {
+  portfolioFilters.forEach(function(filter) {
+    filter.classList.add('disabled');
+    filter.style.pointerEvents = 'none';
+  });
+}
+
+// Mengaktifkan filter setelah interaksi "OK"
+function enablePortfolioFilters() {
+  portfolioFilters.forEach(function(filter) {
+    filter.classList.remove('disabled');
+    filter.style.pointerEvents = 'auto';
+  });
+}
+
+// Memanggil fungsi untuk menonaktifkan filter pada awal
+disablePortfolioFilters();
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Kode peringatan seperti sebelumnya...
+  
+  var okButton = document.querySelector('.warning-content button');
+
+  okButton.addEventListener('click', function() {
+    // Menampilkan konten utama setelah interaksi "OK"
+    var content = document.getElementById('portfolio');
+    content.style.display = 'block';
+  
+    // Aktifkan filter portofolio
+    enablePortfolioFilters();
+  
+    // Terapkan filter portofolio yang sudah dipilih pada peringatan
+    applyPortfolioFilter(selectedFilter);
+  });
+});
+
+// Apply the portfolio filter based on the selected category
+function applyPortfolioFilter(filter) {
+  var portfolioItems = document.querySelectorAll('.portfolio-item');
+
+  // Loop through each portfolio item and apply filter
+  portfolioItems.forEach(function(item) {
+    if (filter === '*' || item.classList.contains(filter.replace('.', ''))) {
+      item.style.display = 'block'; // Show the matching item
+    } else {
+      item.style.display = 'none'; // Hide the non-matching item
+    }
+  });
+}
+
+// Filter click event listener
+document.addEventListener('click', function(event) {
+  if (event.target && event.target.matches('#portfolio-flters li[data-filter]')) {
+    var filter = event.target.getAttribute('data-filter');
+    selectedFilter = filter; // Update the selected filter
+    applyPortfolioFilter(filter);
+  }
+});
--- js/warning.js
+++ js/warning.js
@@ -1,30 +1,78 @@
-window.addEventListener('load', function() {
+// warning.js
+document.addEventListener('DOMContentLoaded', function() {
   var warningContainer = document.createElement('div');
   warningContainer.className = 'warning-container';
+  warningContainer.style.display = 'block';
+  document.body.appendChild(warningContainer);
 
   var warningContent = document.createElement('div');
   warningContent.className = 'warning-content';
+  warningContainer.appendChild(warningContent);
 
   var warningHeading = document.createElement('h2');
   warningHeading.innerText = 'Warning';
+  warningContent.appendChild(warningHeading);
 
   var warningMessage = document.createElement('p');
-  warningMessage.innerText = 'Halaman ini sedang lagi penambahan portfolio project dari pekerjaan-pekerjaan sebelumnya, jika ingin memastikan bisa hubungi melalui linkedin saya, terima kasih.';
+  warningMessage.innerText = 'Halaman ini sedang dalam penambahan portfolio project dari pekerjaan-pekerjaan sebelumnya. Jika ingin memastikan, silakan hubungi melalui LinkedIn saya. Terima kasih.';
+  warningContent.appendChild(warningMessage);
 
   var okButton = document.createElement('button');
   okButton.innerText = 'OK';
+  warningContent.appendChild(okButton);
+
+  var isFilterEnabled = false;
+  var selectedFilter = '*'; // Default filter
+
   okButton.addEventListener('click', function() {
     warningContainer.style.display = 'none';
+    isFilterEnabled = true; // Enable the portfolio filter functionality
+    applyPortfolioFilter(selectedFilter); // Apply the selected filter
     var aboutSection = document.getElementById('about');
     aboutSection.scrollIntoView({ behavior: 'smooth' });
   });
 
-  warningContent.appendChild(warningHeading);
-  warningContent.appendChild(warningMessage);
-  warningContent.appendChild(okButton);
-  warningContainer.appendChild(warningContent);
+  okButton.addEventListener('click', function() {
+    warningContainer.style.display = 'none';
+    isFilterEnabled = true; // Enable the portfolio filter functionality
+    applyPortfolioFilter(selectedFilter); // Apply the selected filter
+    var aboutSection = document.getElementById('about');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+  
+    // Dispatch a custom event to signal that the warning has been acknowledged
+    var event = new Event('warningAcknowledged');
+    document.dispatchEvent(event);
+  });
+  
+  
+  
+  // Apply the portfolio filter
+  function applyPortfolioFilter(filter) {
+    if (isFilterEnabled) {
+      var portfolioItems = document.querySelectorAll('.portfolio-item');
 
-  document.body.appendChild(warningContainer);
+      // Loop through each portfolio item and apply filter
+      portfolioItems.forEach(function(item) {
+        if (filter === '*' || item.classList.contains('filter-' + filter)) {
+          item.style.display = 'block'; // Show the matching item
+        } else {
+          item.style.display = 'none'; // Hide the non-matching item
+        }
+      });
+    }
+  }
 
-  warningContainer.style.display = 'block';
+  // Filter click event listener
+  document.addEventListener('click', function(event) {
+    if (event.target && event.target.matches('#portfolio-flters ul')) {
+      if (!isFilterEnabled) {
+        // Jika filter belum aktif, menyimpan filter yang dipilih untuk digunakan nanti
+        selectedFilter = event.target.getAttribute('data-filter');
+      } else {
+        // Jika filter sudah aktif, menerapkan filter portofolio yang dipilih
+        var filter = event.target.getAttribute('data-filter');
+        applyPortfolioFilter(filter);
+      }
+    }
+  });
 });
